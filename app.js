6290b59f236f7899161cd15bce57524642aa658d
Siddhartha <siddharthac@iitbhilai.ac.in>
2020-11-08 19:11:07 +0530

    Add Listening Port

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.get("/posts/:postname", (req, res) => {
 	} else res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Server started on port 3000");
 });
